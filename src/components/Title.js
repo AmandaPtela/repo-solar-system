84b241b0c9d4c80f98588a6547e2177122aa0b09
Amanda Portela <mandaptela@gmail.com>
2022-05-23 19:36:47 -0300

    requisito 03 e 04 ok

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../App.css';
 
 class Title extends React.Component {
   render() {
+    const { headline } = this.props;
     return (
       <div data-testid="solar-system">
-        <h2 data-testid="solar-system">{ this.props.headline }</h2>
+        <h2 data-testid="solar-system">{ headline }</h2>
       </div>
     );
   }
 }
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 export default Title;
