2ac6e695d540fa2f54680f44227db8be70e673f7
Amanda Portela <mandaptela@gmail.com>
2022-05-23 23:41:38 -0300

    100%

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -2,23 +2,26 @@ import React from 'react';
 import '../App.css';
 import PropTypes from 'prop-types';
 
-class missionCard extends React.Component {
+class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">{name}</p>
-        <p data-testid="mission-year">{year}</p>
-        <p data-testid="mission-country">{country}</p>
-        <p data-testid="mission-destination">{destination}</p>
+      <div className="div-missoes" data-testid="mission-card">
+        <p className="item" id="nome-missao" data-testid="mission-name">{name}</p>
+        <hr />
+        <p className="item" id="ano-missao" data-testid="mission-year">{year}</p>
+        <hr />
+        <p className="item" id="pais-missao" data-testid="mission-country">{country}</p>
+        <hr />
+        <p className="item" id="dest" data-testid="mission-destination">{destination}</p>
       </div>
     );
   }
 }
-missionCard.propTypes = {
+MissionCard.propTypes = {
   name: PropTypes.string.isRequired,
   year: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
   destination: PropTypes.string.isRequired,
 };
-export default missionCard;
+export default MissionCard;
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 import '../App.css';
 
 class missoes extends React.Component {
   render() {
     return (
-      <div className="titulo-missoes" data-testid="missions">
+      <div className="missoes-titulo" data-testid="missions">
         <Title headline="Missões" />
+        <div className="missoes">
+          {missions.map((item) => (
+            <MissionCard
+              key={ item.name }
+              name={ item.name }
+              year={ item.year }
+              country={ item.country }
+              destination={ item.destination }
+            />
+          ))}
+        </div>
       </div>
     );
   }
